c28b530cddbcdd5f0c86f4b1e7348405d0ea9a37
Gaizka-Dev <gaizka_dev@proton.me>
2025-12-05 01:35:51 +0100

    Corregir valores

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -42,7 +42,7 @@ class TestApi(unittest.TestCase):
             response.status, http.client.OK, f"Error en la petición API a {url}"
         )
         self.assertEqual(
-            response.read().decode(), "9", "ERROR ADD"
+            response.read().decode(), "9", "ERROR MULTIPLY"
         )
 
     def test_api_divide(self):
@@ -52,7 +52,7 @@ class TestApi(unittest.TestCase):
             response.status, http.client.OK, f"Error en la petición API a {url}"
         )
         self.assertEqual(
-            response.read().decode(), "4.0", "ERROR ADD"
+            response.read().decode(), "4", "ERROR DIVIDE"
         )        
 
 if __name__ == "__main__":  # pragma: no cover
